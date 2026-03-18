a7db0034275e98df6d4c8ac6d1e93d5b53751ca3
ellango2612 <xuanthu.2612.40@gmail.com>
2026-03-18 16:59:16 -0700

    yes

--- 876. Middle of the Linked List.py
+++ 876. Middle of the Linked List.py
@@ -1,2 +1,4 @@
 # Given the head of a singly linked list, return the middle node of the linked list.
+# If there are two middle nodes, return the second middle node.
 
+# ez: fast and slow pointers. rmb to check None cases (head == None and fast == None)
