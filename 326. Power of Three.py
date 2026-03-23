ba54ba8782cf48cf3a214d2095f23753315fe78e
ellango2612 <xuanthu.2612.40@gmail.com>
2026-03-22 17:00:52 -0700

    ..

---
+++ 326. Power of Three.py
@@ -0,0 +1,11 @@
+# Given an integer n, return true if it is a power of three. Otherwise, return false.
+# An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+def powerofthree(n):
+    if n <= 0:
+        return False
+    if n == 1:
+        return True
+    if n%3 != 0:
+        return False
+    return powerofthree(n//3)
